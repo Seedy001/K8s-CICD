09f5ed124157c9721f0ea0c6bbee13ee38408aaf
NDIAYE sidy <ndiaye.sidy@uam.edu.sn>
2026-09-05 14:19:06 +0000

    V3: live demo failover

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 import os
 import socket
 
-app = FastAPI(title="Demo App", version=os.getenv("APP_VERSION", "v1"))
+app = FastAPI(title="Demo App", version=os.getenv("APP_VERSION", "v2"))
 
 
 @app.get("/")
 def root():
     return {
-        "message": "🎉 Hello from demo-app V2 - survived node failure!",
-        "version": os.getenv("APP_VERSION", "v2"),
+        "message": "🎉 Hello from demo-app V3 - survived node failure!",
+        "version": os.getenv("APP_VERSION", "v3"),
         "pod": socket.gethostname(),
     }
 
